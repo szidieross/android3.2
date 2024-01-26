6cced43774171bddf1a6605b85f47ed6de18cea3
szidieross <szidieross@gmail.com>
2024-01-26 21:01:59 +0200

    idk

--- app/src/main/java/com/example/android32/Product.java
+++ app/src/main/java/com/example/android32/Product.java
@@ -5,6 +5,15 @@ public class Product {
     private double price;
 
     public Product(String name,double price){
+        this.name = name;
+        this.price = price;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
