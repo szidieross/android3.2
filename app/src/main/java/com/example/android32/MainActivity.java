908ed2af89040a1529e7243a883ea219f1a3ccc0
szidieross <szidieross@gmail.com>
2024-01-26 22:31:15 +0200

    done

--- app/src/main/java/com/example/android32/MainActivity.java
+++ app/src/main/java/com/example/android32/MainActivity.java
@@ -3,12 +3,80 @@ package com.example.android32;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Product> productList = new ArrayList<>();
+    private EditText code, name, price;
+    private TextView productsTextView;
+    private Button cancel, add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        code = findViewById(R.id.codeInput);
+        name = findViewById(R.id.nameInput);
+        price = findViewById(R.id.priceInput);
+        cancel = findViewById(R.id.cancel);
+        add = findViewById(R.id.add);
+        productsTextView = findViewById(R.id.products);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearField();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProduct();
+            }
+        });
+    }
+
+    private void clearField() {
+        code.setText("");
+        name.setText("");
+        price.setText("");
+    }
+
+    private void addProduct() {
+        String codeText = code.getText().toString();
+        String nameText = name.getText().toString();
+        double priceText;
+
+        try {
+            priceText = Double.parseDouble(price.getText().toString());
+        }catch(NumberFormatException e){
+            priceText=0.0;
+        }
+
+        Product product = new Product(codeText, nameText, priceText);
+
+        productList.add(product);
+        updateProducts();
+
+        clearField();
+    }
+
+    private void updateProducts(){
+        String productsText = "";
+        for (Product product : productList) {
+            productsText += "Code: " + product.getCode() + " - ";
+            productsText += "Name: " + product.getName() + " - ";
+            productsText += "Price: $" + product.getPrice() + "\n";
+        }
+        productsTextView.setText(productsText);
     }
 }
--- app/src/main/java/com/example/android32/Product.java
+++ app/src/main/java/com/example/android32/Product.java
@@ -1,19 +1,25 @@
 package com.example.android32;
 
 public class Product {
+    private String code;
     private String name;
     private double price;
 
-    public Product(String name,double price){
+    public Product(String code, String name, double price) {
+        this.code = code;
         this.name = name;
         this.price = price;
     }
 
+    public String getCode() {
+        return this.code;
+    }
+
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public double getPrice() {
-        return price;
+        return this.price;
     }
 }
